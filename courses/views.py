85093b30ec86624ee94ea2fcf72d797358ebe2b7
Andy Kim <kimplausible127@gmail.com>
2021-05-08 16:10:58 -0700

    Created Course delete, list, and create templates and set up urls. Also added permissions for views

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, \
+                                       PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -17,9 +19,11 @@ class OwnerEditMixin(object):
         return super().form_valid(form)
 
 
-class OwnerCourseMixin(OwnerMixin):
+class OwnerCourseMixin(OwnerMixin,
+                       LoginRequiredMixin,
+                       PermissionRequiredMixin):
     model = Course
-    fields = ['subjec', 'title', 'slug', 'overview']
+    fields = ['subject', 'title', 'slug', 'overview']
     success_url = reverse_lazy('manage_course_list')
 
 
@@ -32,20 +36,22 @@ class ManageCourseListView(OwnerCourseMixin, ListView):
 
 
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
-    pass
+    permission_required = 'courses.add_course'
 
 
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-    pass
+    permission_required = 'courses.change_course'
 
 
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
+    permission_required = 'courses.delete_course'
 
 
-class ManageCourseListView(ListView):
-    model = Course
-       template_name = 'courses/manage/course/list.html'
-       def get_queryset(self):
-           qs = super().get_queryset()
-           return qs.filter(owner=self.request.user)
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    # model = Course
+    template_name = 'courses/manage/course/list.html'
+    permission_required = 'courses.view_course'
+    # def get_queryset(self):
+    #     qs = super().get_queryset()
+    #     return qs.filter(owner=self.request.user)
